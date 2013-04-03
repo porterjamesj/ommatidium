a0c276a5ba36cbe4c87821310a19c56042d08549
James Porter <porterjamesj@gmail.com>
2013-04-03 00:06:53 -0500

    getting close on the R_25 levels

--- omm.py
+++ omm.py
@@ -31,14 +31,23 @@ def oneDdiff(concs):
 progfxn = interpolate.interp1d(progs[0],progs[1])
 
 #parameter definitions
-# use y_k to change yan kinetics
+
 y_k = 0.5
 y_basal = 0.2
+Y = 1.0
+ny = 2.0
+
 e_k = 0.01
+E = 0.33
+ne = 4.0
+e_prod = 1.3
+D_e = 0.01
 
-params = [y_k,y_basal,e_k]
+r25_k = 1.0
 
-nmol = 2
+params = [y_k,y_basal,e_k,E,ne,D_e,r25_k,Y,ny]
+
+nmol = 3
 
 # The function that determines the rates of change, which we solve to integrate
 def f(y, t, nmol, params):
@@ -46,6 +55,12 @@ def f(y, t, nmol, params):
     y_k = params[0]
     y_basal = params[1]
     e_k = params[2]
+    E = params[3]
+    ne = params[4]
+    D_e = params[5]
+    r25_k = params[6]
+    Y = params[7]
+    ny = params[8] 
     
     #reshape flat array into shape (nmol,5)
     c = np.reshape(y,[nmol,5])
@@ -54,15 +69,15 @@ def f(y, t, nmol, params):
 
     yan = c[0,:]
     egfr = c[1,:]
-    #R_25 = c[2,:]
+    R_25 = c[2,:]
     #R_34 = c[3,:]
 
-    #rates of change for yan
-    xprime[0,:] = y_k * (y_basal + (1-hill(egfr,0.33,4.0))*((progfxn(t)-y_basal)) - yan)
+    # rates of change for yan
+    xprime[0,:] = y_k * (y_basal + (1-hill(egfr,E,ne))*(1-hill(yan,Y,ny))*(progfxn(t)-y_basal) - yan)
     # rates of change for egfr
-    xprime[1,:] = e_k * (0.8 - egfr)
+    xprime[1,:] = e_k * (e_prod*(sens+R_25 - egfr)) + D_e*oneDdiff(egfr)
     # rate of change for R_25
-    #xprime[5,:] = 0.0 #r25_prod + hill(egfr/E_r,1.0,npr)*rough*(1-hill(yan/Y_r,1.0,nyr)) - r25_deg * R_25
+    xprime[2,:] = r25_k * (rough*hill(egfr,0.2,4.0)*(1-hill(yan,1.0,2.0)) - 1.0*R_25)
     # rate of change for R_34
     #xprime[6,:] = 0.0 #r34_prod + hill(p1+pp2/E_r,1.0,npr)*(1-hill((sens+rough)/RS_r,1.0,nrr))*(1-hill(yan/Y_r,1.0,nyr)) - r34_deg * R_34
     return xprime.flatten()
@@ -86,15 +101,18 @@ print "done"
 
 # plot simulation and data
 plt.clf()
-plt.plot(timerange,resols[:,0,2])
-plt.plot(progs[0],progs[1], marker = 'o')
+plt.plot(timerange,resols[:,0,1])
+#plt.plot(progs[0],progs[1], marker = 'o')
 plt.plot(r8s[0],r8s[1], marker = 'o')
-#plt.plot(r25s[0],r25s[1], marker = 'o')
+plt.plot(r25s[0],r25s[1], marker = 'o')
 #plt.plot(r34s[0],r34s[1], marker = 'o')
 plt.axis([0,300,0,1.5])
 plt.savefig("fig.png")
 
 
+
+
+'''
 # emcee parameter tuning
 
 # The function we are trying to minimize
@@ -123,3 +141,4 @@ sampler = emcee.EnsembleSampler(nwalkers, ndim, invrmse, args=[nmol,progs], thre
 # trying scipy.optimize.minimize
 
 progprod = interplolate
+'''
